f500cd82c664d972cb911db09277e29312c29a9b
Abraham García <abrahamgarcar@hotmail.com>
2021-01-29 12:54:57 -0700

    Arreglos en carpeta public

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import Server from './classes/server';
 import router from './routes/router';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import express from 'express';
+
 
 const path = require('path');
 
@@ -10,6 +12,7 @@ const path = require('path');
 const server = Server.instance;
 
 // BodyParser
+server.app.use( express.static('public') );
 server.app.use( bodyParser.urlencoded({ extended: true }) );
 server.app.use( bodyParser.json() );
 
